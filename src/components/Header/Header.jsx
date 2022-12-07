af9df5f4b248813ceb522c3c7e62279af880ac3f
Gudkova Mariia <gudmary8@gmail.com>
2022-12-07 20:55:09 +0400

    add main header for all page sizes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 import headerLogo from "../../images/header__logo.svg";
 import headerButtonIcon from "../../images/header__button-icon.svg";
-import "./Header.css";
 import Navigation from "../Navigation/Navigation.jsx";
+import { routes } from "../../utils/routes";
 
 function Header(props) {
+  const { open, setOpen } = props;
+  const close = () => setOpen(false);
+
+  function onClick() {
+    return setOpen(!open);
+  }
+
   return (
     <header className="header">
       <div className="header__container">
-        <img className="header__logo" src={headerLogo} alt="Logo" />
-        <Navigation />
+        <Link to={routes.baseRoute}>
+          <img className="header__logo" src={headerLogo} alt="Logo" />
+        </Link>
+        <Navigation open={open} />
       </div>
-      <button className="header__button">
+      <Link
+        to={routes.profile}
+        className={!open ? "header__button" : "header__button_active"}
+      >
         <p className="header__button-text">Аккаунт</p>
         <img
           className="header__button-icon"
           src={headerButtonIcon}
           alt="Account icon"
         />
-      </button>
+      </Link>
+      <ul
+        className={!open ? "header__burger_active" : "header__burger"}
+        onClick={onClick}
+      >
+        <li className="header__burger-line"></li>
+        <li className="header__burger-line"></li>
+        <li className="header__burger-line"></li>
+      </ul>
+      <div className={!open ? "header__menu" : "header__menu_active"}>
+        <ul className="header__cross" onClick={close}>
+          <li className="header__cross-line"></li>
+          <li className="header__cross-line"></li>
+        </ul>
+      </div>
     </header>
   );
 }
