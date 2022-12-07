1195130d55250f64afa2a628262540a527793b60
Gudkova Mariia <gudmary8@gmail.com>
2022-12-07 20:55:26 +0400

    add navigation for all page sizes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
+import { routes } from "../../utils/routes";
 
 function Navigation(props) {
+  const { open } = props;
   return (
-    <nav class="navigation">
+    <nav className={!open ? "navigation" : "navigation_active"}>
       <ul className="navigation__list">
-        <li className="navigation__item">
-          <a href="#" className="navigation__link navigation__link_indent">
-            Фильмы
-          </a>
+        <li className="navigation__item navigation__burger-item navigation__burger-item_active">
+          <Link
+            to={routes.baseRoute}
+            className="navigation__link navigation__link_indent"
+          >
+            Главная
+          </Link>
         </li>
         <li className="navigation__item">
-          <a href="#" className="navigation__link">
-            Сохраненные фильмы
-          </a>
+          <Link
+            to={routes.movies}
+            className="navigation__link navigation__link_indent"
+          >
+            Фильмы
+          </Link>
+        </li>
+        <li className="navigation__item">
+          <Link to={routes.savedMovies} className="navigation__link">
+            Сохранённые фильмы
+          </Link>
         </li>
       </ul>
     </nav>
