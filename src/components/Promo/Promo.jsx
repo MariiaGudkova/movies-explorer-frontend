f443e7109bfcc82557e44aa4b5280f446e183b9e
Gudkova Mariia <gudmary8@gmail.com>
2022-12-08 19:36:52 +0400

    delete test paragraph in promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -6,22 +6,15 @@ import promoImage from "../../images/promo__image.svg";
 function Promo(props) {
   const { isLogged } = props;
   return (
-    <>
-      <div className="promo">
-        <Header isLogged={isLogged} />
-        <div className="promo__container">
-          <h1 className="promo__title">
-            Учебный проект студента факультета Веб-разработки.
-          </h1>
-          <img className="promo__image" src={promoImage} alt="Abstract waves" />
-        </div>
+    <div className="promo">
+      <Header isLogged={isLogged} />
+      <div className="promo__container">
+        <h1 className="promo__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+        <img className="promo__image" src={promoImage} alt="Abstract waves" />
       </div>
-      <p>
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-        Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum
-      </p>
-    </>
+    </div>
   );
 }
 
