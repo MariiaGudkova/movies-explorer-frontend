d1825ab8e30c1e3de4558b2ab009e6bbe32dbd12
Gudkova Mariia <gudmary8@gmail.com>
2022-12-08 23:57:45 +0400

    rename blocks according to BEM

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -7,32 +7,32 @@ function AboutProject() {
       <div className="about-project__container">
         <h2 className="about-project__title">О проекте</h2>
         <div className="paragraphs-container">
-          <div className="about-project__paragraph">
-            <h3 className="about-project__subtitle">
+          <div className="paragraph">
+            <h3 className="paragraph__title">
               Дипломный проект включал 5 этапов
             </h3>
-            <p className="about-project__text">
+            <p className="paragraph__text">
               Составление плана, работу над бэкендом, вёрстку, добавление
               функциональности и финальные доработки.
             </p>
           </div>
-          <div className="about-project__paragraph">
-            <h3 className="about-project__subtitle">
+          <div className="paragraph">
+            <h3 className="paragraph__title">
               На выполнение диплома ушло 5 недель
             </h3>
-            <p className="about-project__text">
+            <p className="paragraph__text">
               У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
               соблюдать, чтобы успешно защититься.
             </p>
           </div>
         </div>
-        <div className="about-project__scale">
-          <div className="about-project__scale-segment about-project__scale-segment_short">
-            <p className="about-project__segment_back">1 неделя</p>
+        <div className="timescale">
+          <div className="timescale__segment timescale__segment_short">
+            <p className="timescale__description_back">1 неделя</p>
             <p>Back-end</p>
           </div>
-          <div className="about-project__scale-segment about-project__scale-segment_lang">
-            <p className="about-project__segment_front">4 недели</p>
+          <div className="timescale__segment timescale__segment_long">
+            <p className="timescale__description_front">4 недели</p>
             <p>Front-end</p>
           </div>
         </div>
