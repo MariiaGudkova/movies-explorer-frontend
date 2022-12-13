3290ebf783b48bf664245668302ea545bffaa9f5
Gudkova Mariia <gudmary8@gmail.com>
2022-12-13 20:04:39 +0400

    change for saved movies page

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,17 +1,23 @@
 import "./MoviesCard.css";
 import checkMark from "../../images/checkmark.svg";
+import deleteIcon from "../../images/delete-icon.svg";
 
 function MoviesCard(props) {
-  const { movie } = props;
-  const saveButtonClassName = `movie__save-button ${
+  const { movie, isPageSavedMovies } = props;
+  let saveButtonClassName = `movie__save-button ${
     movie.isSaved ? "movie__save-button_active" : ""
   }`;
-  const saveButtonText =
+  let saveButtonText =
     saveButtonClassName === "movie__save-button movie__save-button_active" ? (
       <img src={checkMark} alt="Checkmark" />
     ) : (
       "Сохранить"
     );
+
+  if (isPageSavedMovies) {
+    saveButtonClassName = "movie__delete-button";
+    saveButtonText = <img src={deleteIcon} alt="Cross" />;
+  }
 
   return (
     <li className="movie">
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,12 +2,18 @@ import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard.jsx";
 
 function MoviesCardList(props) {
-  const { movies } = props;
+  const { movies, isPageSavedMovies } = props;
   return (
     <div className="movies">
       <ul className="movies__list">
         {movies.map((movie) => {
-          return <MoviesCard movie={movie} key={movie._id} />;
+          return (
+            <MoviesCard
+              movie={movie}
+              key={movie._id}
+              isPageSavedMovies={isPageSavedMovies}
+            />
+          );
         })}
       </ul>
       <button className="movies__button" type="button" onClick={() => {}}>
