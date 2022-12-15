ddcccb2c7304d3364c6f73819a7179038c8d201f
Gudkova Mariia <gudmary8@gmail.com>
2022-12-15 12:24:56 +0400

    review fix:  make portfolio__item single link

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,13 +8,13 @@ function Portfolio() {
         <h2 className="portfolio__title">Портфолио</h2>
         <ul className="portfolio__list">
           <li className="portfolio__item">
-            <p className="portfolio__subtitle">Сервис Mesto</p>
             <a
               href="https://github.com/MariiaGudkova/react-mesto-api-full"
               className="portfolio__link"
               target="_blank"
               rel="noreferrer"
             >
+              <p className="portfolio__subtitle">Сервис Mesto</p>
               <img
                 className="portfolio__arrow"
                 src={portfolioArrow}
@@ -23,13 +23,13 @@ function Portfolio() {
             </a>
           </li>
           <li className="portfolio__item">
-            <p className="portfolio__subtitle">Мессенджер</p>
             <a
               href="https://github.com/MariiaGudkova/Messenger_front_Itransition"
               className="portfolio__link"
               target="_blank"
               rel="noreferrer"
             >
+              <p className="portfolio__subtitle">Мессенджер</p>
               <img
                 className="portfolio__arrow"
                 src={portfolioArrow}
@@ -38,13 +38,15 @@ function Portfolio() {
             </a>
           </li>
           <li className="portfolio__item portfolio__item_no-border">
-            <p className="portfolio__subtitle">Сервис удаления и блокировки</p>
             <a
               href="https://github.com/MariiaGudkova/Web_application_Itransition"
               className="portfolio__link"
               target="_blank"
               rel="noreferrer"
             >
+              <p className="portfolio__subtitle">
+                Сервис удаления и блокировки
+              </p>
               <img
                 className="portfolio__arrow"
                 src={portfolioArrow}
