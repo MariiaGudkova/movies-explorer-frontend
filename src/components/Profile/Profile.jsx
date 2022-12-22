5784db465a54694cfc6f233b55df7ac97f4497f4
Gudkova Mariia <gudmary8@gmail.com>
2022-12-22 15:00:31 +0400

    add profile form validation

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,123 @@
+import React from "react";
 import "./Profile.css";
+import { useFormWithValidation } from "../../hooks/useFormWithValidation.js";
 
 function Profile(props) {
-  const { userData, onLogout } = props;
+  const { userData, nameRegex, emailRegex, onLogout } = props;
+  let [isDisabled, setIsDisabled] = React.useState(true);
+  let [isChangeButtons, setIsChangeButtons] = React.useState(true);
+  let [isSavedButton, setIsSavedButton] = React.useState(false);
+  const { values, setValues, handleChange, errors, isValid, resetForm } =
+    useFormWithValidation({});
+  let saveButtonClass = isValid
+    ? "profile-form__save-button"
+    : "profile-form__save-button profile-form__save-button_disabled";
+
+  if (isSavedButton) {
+    saveButtonClass = isValid
+      ? "profile-form__save-button_active"
+      : "profile-form__save-button_active profile-form__save-button_disabled";
+  }
+
+  React.useEffect(() => {
+    setValues(userData);
+  }, [setValues, userData]);
+
+  function onClick() {
+    setIsDisabled(false);
+    setIsChangeButtons(false);
+    setIsSavedButton(true);
+  }
+
   return (
     <main className="profile">
-      <div className="profile__container">
-        <h1 className="profile__title">
-          Привет, {userData.name ? userData.name : "Пользователь"}!
-        </h1>
-        <div className="user-data">
-          <div className="user-data__item user-data__item_name">
-            <p className="user-data__title">Имя</p>
-            <p className="user-data__text">
-              {userData.name ? userData.name : "Пользователь"}
-            </p>
+      <form className="profile-form">
+        <h1 className="profile-form__title">Привет, {userData.name}!</h1>
+        <fieldset className="profile-form__fieldset">
+          <div className="profile-form__container">
+            <label className="profile-form__label" htmlFor="name-profile-input">
+              Имя
+            </label>
+            <input
+              className={
+                !errors["name"]
+                  ? "profile-form__input profile-form__input_name"
+                  : "profile-form__input profile-form__input_error profile-form__input_name"
+              }
+              id="name-profile-input"
+              type="text"
+              name="name"
+              value={values.name || ""}
+              required
+              minLength="2"
+              maxLength="30"
+              pattern={nameRegex}
+              disabled={isDisabled}
+              onChange={handleChange}
+            />
+            <span
+              className={
+                isValid
+                  ? "profile-form__input-error"
+                  : "profile-form__input-error profile-form__input-error_active"
+              }
+            >
+              {errors["name"]}
+            </span>
           </div>
-          <div className="user-data__item">
-            <p className="user-data__title">E-mail</p>
-            <p className="user-data__text">{userData.email}</p>
+          <div className="profile-form__container profile-form__container_no-border">
+            <label
+              className="profile-form__label"
+              htmlFor="email-profile-input"
+            >
+              E-mail
+            </label>
+            <input
+              className={
+                !errors["email"]
+                  ? "profile-form__input"
+                  : "profile-form__input profile-form__input_error"
+              }
+              id="email-profile-input"
+              type="email"
+              name="email"
+              value={values.email || ""}
+              required
+              minLength="6"
+              maxLength="64"
+              pattern={emailRegex}
+              disabled={isDisabled}
+              onChange={handleChange}
+            />
+            <span
+              className={
+                isValid
+                  ? "profile-form__input-error"
+                  : "profile-form__input-error profile-form__input-error_active"
+              }
+            >
+              {errors["email"]}
+            </span>
           </div>
-        </div>
-        <div className="profile__buttons">
+        </fieldset>
+        <div
+          className={
+            isChangeButtons
+              ? "profile-form__buttons_active"
+              : "profile-form__buttons"
+          }
+        >
           <button
-            className="profile__button-item profile__button-item_edit"
+            className="profile-form__button profile-form__button_edit"
             type="button"
-            onClick={() => {}}
+            onClick={() => {
+              onClick();
+            }}
           >
             Редактировать
           </button>
           <button
-            className="profile__button-item profile__button-item_exit"
+            className="profile-form__button profile-form__button_exit"
             type="button"
             onClick={() => {
               onLogout();
@@ -38,7 +126,11 @@ function Profile(props) {
             Выйти из аккаунта
           </button>
         </div>
-      </div>
+        <span className="profile-form__server-error"></span>
+        <button className={saveButtonClass} type="submit" disabled={!isValid}>
+          Сохранить
+        </button>
+      </form>
     </main>
   );
 }
