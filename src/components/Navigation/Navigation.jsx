6a05123dc7c9ccc2c102e27a68198113ac5186ce
Gudkova Mariia <gudmary8@gmail.com>
2022-12-28 13:27:41 +0400

    add active page selection

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navigation.css";
 import { routes } from "../../utils/routes";
 
 function Navigation(props) {
   const { open, onClick } = props;
-
+  const location = useLocation();
+  const isMoviesActive = location.pathname === routes.movies;
+  const isSavedMoviesActive = location.pathname === routes.savedMovies;
   function handleClick() {
     onClick();
   }
+
   return (
     <nav className={!open ? "navigation" : "navigation navigation_active"}>
       <ul className="navigation__list">
@@ -24,7 +27,11 @@ function Navigation(props) {
         <li className="navigation__item">
           <Link
             to={routes.movies}
-            className="navigation__link navigation__link_indent"
+            className={
+              isMoviesActive
+                ? "navigation__link navigation__link_active navigation__link_indent"
+                : "navigation__link navigation__link_indent"
+            }
             onClick={() => handleClick()}
           >
             Фильмы
@@ -33,7 +40,11 @@ function Navigation(props) {
         <li className="navigation__item">
           <Link
             to={routes.savedMovies}
-            className="navigation__link"
+            className={
+              isSavedMoviesActive
+                ? "navigation__link navigation__link_active"
+                : "navigation__link"
+            }
             onClick={() => handleClick()}
           >
             Сохранённые фильмы
