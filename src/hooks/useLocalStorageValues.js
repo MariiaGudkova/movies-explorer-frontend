cbd86efd1b44e2b58dd569a6cc7e55ed50a271b7
Gudkova Mariia <gudmary8@gmail.com>
2022-12-31 02:35:54 +0400

    change localstorage hook

--- src/hooks/useLocalStorageValues.js
+++ src/hooks/useLocalStorageValues.js
@@ -1,17 +1,13 @@
 import { useState, useEffect } from "react";
 
 function getStorageValue(key, defaultValue) {
-  if (typeof window !== "undefined") {
     const saved = localStorage.getItem(key);
-    const initial = saved !== null ? JSON.parse(saved) : defaultValue;
+    const initial = saved || defaultValue;
     return initial;
-}
 }
 
 export const useLocalStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {
-    return getStorageValue(key, defaultValue);
-  });
+  const [value, setValue] = useState(defaultValue);
 
   useEffect(() => {
     localStorage.setItem(key, value);
