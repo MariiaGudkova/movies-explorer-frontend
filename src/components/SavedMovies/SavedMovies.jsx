d0ac072366f901fb8a2c25b93df8bbb466b7efa3
Gudkova Mariia <gudmary8@gmail.com>
2023-01-05 15:00:02 +0400

    add saved movies logic to the component

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,51 +1,95 @@
+import React from "react";
 import "./SavedMovies.css";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList.jsx";
 import Preloader from "../Preloader/Preloader.jsx";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
+import {
+  getSavedMovies as getSavedMoviesApi,
+  deleteMovie,
+} from "../../utils/MainApi";
+import { filterMovies, updateStoredItem } from "../../utils/utils";
+import { errors } from "../../utils/constants";
 
-function SavedMovies(props) {
-  const {
-    movies,
-    isLoading,
-    searchFormEmptyErrorText,
-    searchFormNotFoundErrorText,
-    isSearchMovieEmptyError,
-    setIsSearchMovieEmptyError,
-    isSearchMovieNotFoundError,
-    setIsSearchMovieNotFoundError,
-    isChecked,
-    setIsChecked,
-    setSearchString,
-    isSavedMoviesFilter,
-    setIsSavedMoviesFilter,
-    onDeleteMovie,
-  } = props;
-  const isPageSavedMovies = true;
+function SavedMovies() {
+  const currentUser = React.useContext(CurrentUserContext);
+  const [savedMovies, setSavedMovies] = React.useState([]);
+  const [searchedMovies, setSearchedMovies] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [searchError, setSearchError] = React.useState(null);
+
+  React.useEffect(() => {
+    getSavedMovies();
+  }, []);
+
+  const getSavedMovies = async () => {
+    try {
+      const jwt = localStorage.getItem("jwt");
+      const { data: res } = await getSavedMoviesApi(jwt, currentUser._id);
+      setSavedMovies(res);
+    } catch (e) {
+      setSearchError("PROCESSING_ERROR");
+    }
+  };
+
+  const searchMovies = async (searchStr, isShortFilm) => {
+    try {
+      if (!searchStr || searchStr.length === 0) {
+        throw new Error("EMPTY_SEARCH_STRING");
+      }
+      setIsLoading(true);
+      const res = filterMovies(savedMovies, searchStr, isShortFilm);
+      if (res.length === 0) {
+        throw new Error("MOVIES_NOT_FOUND");
+      }
+      setSearchError(null);
+      setSearchedMovies(res);
+    } catch (e) {
+      const err = Object.keys(errors).includes(e.message)
+        ? e.message
+        : "PROCESSING_ERROR";
+      setSearchError(err);
+      setSearchedMovies([]);
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleDeleteMovie = async (movie) => {
+    try {
+      const jwt = localStorage.getItem("jwt");
+      await deleteMovie(jwt, movie._id);
+      const updatedSearchedMovies = searchedMovies.filter(
+        (item) => item._id !== movie._id
+      );
+      const updatedSavedMovies = savedMovies.filter(
+        (item) => item._id !== movie._id
+      );
+      setSearchedMovies(updatedSearchedMovies);
+      setSavedMovies(updatedSavedMovies);
+      updateStoredItem(movie.movieId, false);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <>
       <main className="main-container">
         <SearchForm
-          searchFormEmptyErrorText={searchFormEmptyErrorText}
-          searchFormNotFoundErrorText={searchFormNotFoundErrorText}
-          isSearchMovieEmptyError={isSearchMovieEmptyError}
-          setIsSearchMovieEmptyError={setIsSearchMovieEmptyError}
-          isSearchMovieNotFoundError={isSearchMovieNotFoundError}
-          setIsSearchMovieNotFoundError={setIsSearchMovieNotFoundError}
-          isChecked={isChecked}
-          setIsChecked={setIsChecked}
-          setSearchString={setSearchString}
-          isSavedMoviesFilter={isSavedMoviesFilter}
-          setIsSavedMoviesFilter={setIsSavedMoviesFilter}
-          isPageSavedMovies={isPageSavedMovies}
+          onSubmit={searchMovies}
+          searchString={""}
+          isShortFilm={false}
+          searchError={searchError}
         />
         {isLoading ? (
           <Preloader />
         ) : (
           <MoviesCardList
-            movies={movies}
-            isPageSavedMovies={isPageSavedMovies}
-            onDeleteMovie={onDeleteMovie}
+            movies={searchedMovies}
+            isPageSavedMovies
+            onDeleteMovie={handleDeleteMovie}
           />
         )}
       </main>
