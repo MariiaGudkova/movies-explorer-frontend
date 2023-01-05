f62fdaaad7a8b614b2f632a4d98cb06f955b29fd
Gudkova Mariia <gudmary8@gmail.com>
2023-01-05 15:04:31 +0400

    change search form logic

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,60 +3,31 @@ import "./SearchForm.css";
 import searchLoupe from "../../images/search__logo.svg";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox.jsx";
 import { useFormWithValidation } from "../../hooks/useFormWithValidation.js";
-import { useLocalStorage } from "../../hooks/useLocalStorageValues";
+import { errors } from "../../utils/constants";
 
 function SearchForm(props) {
-  const {
-    searchFormEmptyErrorText,
-    searchFormNotFoundErrorText,
-    isSearchMovieEmptyError,
-    setIsSearchMovieEmptyError,
-    isSearchMovieNotFoundError,
-    setIsSearchMovieNotFoundError,
-    isChecked,
-    setIsChecked,
-    setSearchString,
-    setIsSavedMoviesFilter,
-    isPageSavedMovies,
-  } = props;
-  const { handleChange } = useFormWithValidation({});
-  const [localStorageSearchText, setLocalStorageSearchText] = useLocalStorage(
-    "searchString",
-    ""
-  );
-  const [isLocalStorageChecked, setIsLocalStorageChecked] = useLocalStorage(
-    "isChecked",
-    false
-  );
+  const { onSubmit, searchString, isShortFilm, searchError } = props;
 
-  const isErrorShown = isSearchMovieEmptyError || isSearchMovieNotFoundError;
-  let errorMessage = "";
-  if (isSearchMovieEmptyError) {
-    errorMessage = searchFormEmptyErrorText;
-  } else if (isSearchMovieNotFoundError) {
-    errorMessage = searchFormNotFoundErrorText;
-  }
+  const { values, handleInputChange } = useFormWithValidation({
+    searchString: searchString || "",
+    isShortFilm: isShortFilm || false,
+  });
 
-  function onChange(event) {
-    setIsSearchMovieEmptyError(false);
-    setIsSearchMovieNotFoundError(false);
-    if (!isPageSavedMovies) {
-      setLocalStorageSearchText(event.target.value);
-    }
-    handleChange(event);
-  }
+  const errorMessage = Boolean(searchError) ? (
+    <span className={"search__input-error search__input-error_active"}>
+      {errors[searchError]}
+    </span>
+  ) : null;
 
-  async function onSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (
-      isPageSavedMovies
-        ? setIsSavedMoviesFilter(true)
-        : setIsSavedMoviesFilter(false)
-    );
-    const form = document.querySelector(".search__form");
-    const input = form.querySelector(".search__input");
-    setSearchString(input.value.toLowerCase());
-  }
+    onSubmit(values.searchString, values.isShortFilm);
+  };
+
+  const handleIsShortFilmChange = (event) => {
+    handleInputChange(event);
+    onSubmit(values.searchString, !values.isShortFilm);
+  };
 
   return (
     <section className="search">
@@ -67,7 +38,7 @@ function SearchForm(props) {
           action="#"
           autoComplete="off"
           noValidate
-          onSubmit={onSubmit}
+          onSubmit={handleSubmit}
         >
           <input
             className="search__input"
@@ -75,28 +46,16 @@ function SearchForm(props) {
             type="text"
             placeholder="Фильм"
             required
-            value={!isPageSavedMovies ? localStorageSearchText : undefined}
-            onChange={onChange}
+            value={values.searchString}
+            onChange={handleInputChange}
           />
-          <span
-            className={
-              !isErrorShown
-                ? "search__input-error"
-                : "search__input-error search__input-error_active"
-            }
-          >
-            {errorMessage}
-          </span>
+          {errorMessage}
           <button className="search__button" type="submit">
             <img className="search__logo" src={searchLoupe} alt="Loupe" />
           </button>
           <FilterCheckbox
-            isChecked={isChecked}
-            setIsChecked={setIsChecked}
-            setSearchString={setSearchString}
-            isLocalStorageChecked={isLocalStorageChecked}
-            setIsLocalStorageChecked={setIsLocalStorageChecked}
-            isPageSavedMovies={isPageSavedMovies}
+            value={values.isShortFilm}
+            onChange={handleIsShortFilmChange}
           />
         </form>
       </div>
