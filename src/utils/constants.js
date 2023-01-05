c6186217eb19f210fbbae3aeaef82afcb81ce553
Gudkova Mariia <gudmary8@gmail.com>
2023-01-05 15:00:26 +0400

    change server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 export const BEATFILM_MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
 
-export const USERS_API_URL = "http://localhost:4000";
+export const USERS_API_URL = "https://api.movies-gudkova.nomoredomains.club";
 
-export const searchFormEmptyErrorText = 'Нужно ввести ключевое слово';
-export const searchFormNotFoundErrorText = 'Ничего не найдено';
+export const errors = {
+  EMPTY_SEARCH_STRING: "Нужно ввести ключевое слово",
+  MOVIES_NOT_FOUND: "Ничего не найдено",
+  PROCESSING_ERROR: "Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз",
+}
